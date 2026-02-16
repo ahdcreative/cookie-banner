4bb329dcc05eac11f163b0b96b9e3a2db0f7d89a
Artek <darkartek@gmail.com>
2026-02-16 05:59:17 +0100

    fix: ignore node_modules and configure correct type generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
+import dts from 'vite-plugin-dts';
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({
+    rollupTypes: true,
+    tsconfigPath: './tsconfig.app.json'
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
